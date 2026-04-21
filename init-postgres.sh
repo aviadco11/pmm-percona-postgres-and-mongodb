bd1da590828f58f2cdfe39e85e96c1db0633bc52
aviadco11 <88856648+aviadco11@users.noreply.github.com>
2026-04-21 10:57:03 +0300

    Create init-postgres.sh

---
+++ init-postgres.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Enable pg_stat_statements for query monitoring
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+  CREATE USER ${PMM_CLIENT_USER} WITH ENCRYPTED PASSWORD '${PMM_CLIENT_PASSWORD}';
+  GRANT pg_monitor TO ${PMM_CLIENT_USER};
+EOSQL
+
+
